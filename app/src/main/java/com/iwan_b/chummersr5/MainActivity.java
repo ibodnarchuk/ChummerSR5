3b5f23d9d35ee22d932efb67d56998b2042aaeda
ibodnarchuk <IwanBodnarchuk@gmail.com>
2015-10-06 19:16:33 -0400

    Initial commit

---
+++ app/src/main/java/com/iwan_b/chummersr5/MainActivity.java
@@ -0,0 +1,13 @@
+package com.iwan_b.chummersr5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
