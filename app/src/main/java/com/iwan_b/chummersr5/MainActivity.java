ca4fd188c749015a59ba8a5849fff424fb8b4d7b
ibodnarchuk <IwanBodnarchuk@gmail.com>
2015-10-06 22:38:28 -0400

    Created the main page for the application.

--- app/src/main/java/com/iwan_b/chummersr5/MainActivity.java
+++ app/src/main/java/com/iwan_b/chummersr5/MainActivity.java
@@ -1,8 +1,11 @@
 package com.iwan_b.chummersr5;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Test commit
+
+        Button newCharButton = (Button) findViewById(R.id.new_char_button);
+        Button loadCharButton = (Button) findViewById(R.id.load_char_button);
+        Button exitButton = (Button) findViewById(R.id.exit_button);
+
+        newCharButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "New Character Button Clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        loadCharButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "Load Character Button Clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+
     }
 }
