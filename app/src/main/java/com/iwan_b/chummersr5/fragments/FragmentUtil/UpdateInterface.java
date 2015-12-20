8d6ac515a70b9ff7033d3ba2250aa80531f42bc6
ibodnarchuk <iwanbodnarchuk@gmail.com>
2015-12-20 15:39:20 -0500

    Fixing GIT issue with FragmentUtil
    Fixing GIT issue with FragmentUtil

---
+++ app/src/main/java/com/iwan_b/chummersr5/fragments/FragmentUtil/UpdateInterface.java
@@ -0,0 +1,9 @@
+package com.iwan_b.chummersr5.fragments.FragmentUtil;
+
+public interface UpdateInterface {
+    /**
+     * Used to update the screen when the user switches fragments
+     */
+    void update();
+    void updateParent();
+}
